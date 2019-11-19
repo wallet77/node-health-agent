1cd9d678a3ee028ff345df3ee0c2bcf490b13317
wallet77 <wallet77@gmail.com>
2019-11-19 17:40:55 +0100

    Add meta information + enable linter.

--- index.js
+++ index.js
@@ -3,66 +3,66 @@ const WebSocket = require('ws')
 const Inspector = require('inspector-api')
 
 const heartbeat = (ws) => {
-  clearTimeout(ws.pingTimeout)
- 
-  // Use `WebSocket#terminate()`, which immediately destroys the connection,
-  // instead of `WebSocket#close()`, which waits for the close timer.
-  // Delay should be equal to the interval at which your server
-  // sends out pings plus a conservative assumption of the latency.
-  ws.pingTimeout = setTimeout(() => {
-    ws.terminate()
-  }, 30000 + 1000)
+    clearTimeout(ws.pingTimeout)
+
+    // Use `WebSocket#terminate()`, which immediately destroys the connection,
+    // instead of `WebSocket#close()`, which waits for the close timer.
+    // Delay should be equal to the interval at which your server
+    // sends out pings plus a conservative assumption of the latency.
+    ws.pingTimeout = setTimeout(() => {
+        ws.terminate()
+    }, 30000 + 1000)
 }
 
 const connectToWSS = async (config, inspector) => {
-  const ws = new WebSocket(`${config.serverUrl}/${os.hostname()}/${config.appName}`)
+    const ws = new WebSocket(`${config.serverUrl}/${os.hostname()}/${config.appName}`)
 
-  ws.on('ping', () => {heartbeat(ws)})
+    ws.on('ping', () => { heartbeat(ws) })
 
-  ws.on('open', () => {
-    console.info('Connected to WS server.')
-    heartbeat(ws)
-  })
+    ws.on('open', () => {
+        console.info('Connected to WS server.')
+        heartbeat(ws)
+    })
 
-  ws.on('message', (msg) => {
-    if(!events[msg]) return console.warn(`Event ${msg} not handled!\nYou can use the addEvent() method to attach an action to a specific event.`)
-    events[msg](msg, inspector)
-  })
+    ws.on('message', (msg) => {
+        if (!events[msg]) return console.warn(`Event ${msg} not handled!\nYou can use the addEvent() method to attach an action to a specific event.`)
+        events[msg](msg, inspector)
+    })
 
-  ws.on('close', async () => {
-    ws.terminate()
-    await connectToWSS(config, inspector)
-    clearTimeout(ws.pingTimeout)
-  })
+    ws.on('close', async () => {
+        ws.terminate()
+        await connectToWSS(config, inspector)
+        clearTimeout(ws.pingTimeout)
+    })
 
-  ws.on('error', async (err) => {
-    ws.terminate()
-  })
+    ws.on('error', async (err) => {
+        ws.terminate()
+    })
 }
 
 const events = {
-  'cpu_profiling_start': (message, inspector) => {
-    if (!inspector) return console.warn("Not inspector configuration found!")
-    inspector.profiler.start()
-  },
-  'cpu_profiling_stop': (message, inspector) => {
-    if (!inspector) return console.warn("Not inspector configuration found!")
-    inspector.profiler.stop()
-  }
+    cpu_profiling_start: (message, inspector) => {
+        if (!inspector) return console.warn('Not inspector configuration found!')
+        inspector.profiler.start()
+    },
+    cpu_profiling_stop: (message, inspector) => {
+        if (!inspector) return console.warn('Not inspector configuration found!')
+        inspector.profiler.stop()
+    }
 }
 
 module.exports = (config = {}) => {
-    if(!config.appName) {
+    if (!config.appName) {
         console.error('Can\'t start node health agent, no app name provided!')
         return
     }
-    if(!config.serverUrl) {
+    if (!config.serverUrl) {
         console.error('Can\'t start node health agent, no server url!')
         return
     }
 
     let inspector
-    if(config.inspector) {
+    if (config.inspector) {
         inspector = new Inspector(config.inspector)
         inspector.profiler.enable()
     }
@@ -70,8 +70,8 @@ module.exports = (config = {}) => {
     connectToWSS(config, inspector)
 
     return {
-      addEvent: (event, fn) => {
-        events[event] = fn
-      }
+        addEvent: (event, fn) => {
+            events[event] = fn
+        }
     }
 }
---
+++ readme.md
@@ -0,0 +1,67 @@
+[![GitHub release](https://badge.fury.io/js/node-health-agent.svg)](https://github.com/wallet77/node-health-agent/releases/)
+[![GitHub license](https://img.shields.io/github/license/wallet77/node-health-agent)](https://github.com/wallet77/node-health-agent/blob/master/LICENSE)
+
+# Purpose
+
+Node agent for Node health project.
+
+# Compatibility
+
+**/!\ This module use async/await syntax and the inspector module, this is why you must have node 8.0+.**
+
+Supported and tested : >= 8.0
+
+| Version       | Supported     | Tested         |
+|:-------------:|:-------------:|:--------------:|
+| 12.x          | yes           | yes            |
+| 10.x          | yes           | yes            |
+| 9.x           | yes           | yes            |
+| 8.x           | yes           | yes            |
+
+**In order to have all features we recommend to use at least Node.js version 10 or higher.**
+
+# Installation
+
+```console
+$ npm install node-health-agent --save
+```
+
+# Usage
+
+## Basic
+```javascript
+const agent = require('node-health-agent')({
+  appName: 'testAPI',
+  serverUrl: 'ws://localhost:3001',
+  inspector: {
+    storage: {
+      type: "s3",
+      bucket: process.env.CONFIG_S3_BUCKET,
+      dir: 'inspector'
+    }
+  }
+})
+
+```
+
+## Add a custom event
+```javascript
+agent.addEvent('myEvent', (event) => {
+  console.log(event)
+})
+```
+
+# List of built-in events
+
+| Event                        | description                                |
+|:----------------------------:|:------------------------------------------:|
+| `cpu_profiling_start`        | Start a CPU profiling                      |
+| `cpu_profiling_stop`         | Stop a CPU profiling                       |
+
+# Test
+
+```console
+$ npm test
+```
+
+Coverage report can be found in coverage/.
