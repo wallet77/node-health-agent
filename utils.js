ee8fa0d16254d76dd028c4fadf506a04a53a9844
wallet77 <wallet77@gmail.com>
2022-07-06 15:16:57 +0200

    fix: always test if subDir is a directory

--- utils.js
+++ utils.js
@@ -2,13 +2,23 @@ const fs = require('fs')
 const debug = require('debug')
 const error = debug('node-health-agent:error')
 const path = require('path')
+const isDirectory = (dir) => {
+    try {
+        if (fs.existsSync(dir) && fs.lstatSync(dir).isDirectory()) {
+            return true
+        }
+        return false
+    } catch (err) {
+        return false
+    }
+}
 class Utils {
     extractDependencies (dir) {
         let packages = {}
         try {
             const subDirs = fs.readdirSync(dir)
             subDirs.forEach((subDir) => {
-                if (subDir === '.bin') return
+                if (subDir === '.bin' || !isDirectory(`${dir}/${subDir}`)) return
                 if (subDir.indexOf('@') === 0) {
                     packages = { ...packages, ...this.extractDependencies(`${dir}/${subDir}`) }
                     return
